6ea142992d2428b678d67425a7249686e6158843
Oleg Pinta <oleg.sabfir@gmail.com>
2016-05-23 23:45:47 +0300

    spring boot settings

---
+++ src/main/java/Application.java
@@ -0,0 +1,17 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot main Application
+ *
+ */
+@SpringBootApplication
+public class Application 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
