a3d0148a90d53e12b0b35c2e013fd6acab6c14c4
Oleg Pinta <oleg.sabfir@gmail.com>
2016-05-27 13:41:04 +0300

    logging

--- src/main/java/bootstrap/Application.java
+++ src/main/java/bootstrap/Application.java
@@ -1,6 +1,5 @@
 package bootstrap;
 
-//import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,16 +15,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = { "controller", "helper" })
 @PropertySource("classpath:/settings/application.properties")
 public class Application {
-//	final static Logger logger = Logger.getLogger(Application.class);
-	
 	public static void main( String[] args ) throws Exception {
-//		String parameter = "Oleg!";
-//	    logger.debug("This is debug : " + parameter);
-//		logger.info("This is info : " + parameter);
-//		logger.warn("This is warn : " + parameter);
-//		logger.error("This is error : " + parameter);
-//		logger.fatal("This is fatal : " + parameter);
-	
         SpringApplication.run(Application.class, args);
     }
 }
